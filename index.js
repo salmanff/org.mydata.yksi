1fad54983466cfb9004794628f15ad62df660591
salmanff <1517629+salmanff@users.noreply.github.com>
2022-03-04 20:35:24 +0100

    updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ freezr.initPageScripts = function () {
     if (response && response.text) document.getElementById('the_text').innerText = response.text
   })
     .catch((e) => {
-      if (e.message !== 'no related records') alert('Hmmm - there was an error connecting to your freezr:' + e.message)
+      if (e.message.indexOf('no related records') !== 0) alert('Hmmm - there was an error connecting to your freezr:' + e.message)
     })
 
   document.getElementById('save_button').addEventListener('click', function (e) {
@@ -12,6 +12,7 @@ freezr.initPageScripts = function () {
       { text: document.getElementById('the_text').innerText }, { data_object_id: 'yksiNote', upsert: true }
     )
       .then((response) => {
+        console.log(response)
         alert(response.error ? 'Oops - there was an error saving.' : 'YAAY, you saved your yksi note on your freezr')
       })
       .catch((error) => {
@@ -20,8 +21,9 @@ freezr.initPageScripts = function () {
   }, false)
 
   document.getElementById('publish_button').addEventListener('click', function (e) {
-    freezr.promise.perms.setObjectAccess('yksiNote', { name: 'publish_note', grant: true, grantees: ['_public'], table_id: 'org.mydata.yksi' })
+    freezr.promise.perms.shareRecords('yksiNote', { name: 'publish_note', action: 'grant', grantees: ['_public'], table_id: 'org.mydata.yksi' })
       .then((response) => {
+        console.log(response)
         alert(response.error ? ('Oops - there was an error publishing' + response.error) : 'YAAY, you you just published your yksi note')
         return null
       })
